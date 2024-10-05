a6cb4d586513ccacb1b32940079aa72af3851d78
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-10-04 21:14:31 -0700

    rm

--- repo/push.sh
+++ repo/push.sh
@@ -3,7 +3,8 @@
 
 # git init
 git add .
-git commit -m "Readme update"
+git commit -m "rm
+"
 # git branch -M main
 # git remote add origin https://github.com/aaron-dm-mcdonald/py-awsCLI-boto3-test.git
 git push # -u origin main
